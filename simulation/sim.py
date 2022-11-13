79ca717eb33d7229ea9e1b61e0a8a029a0fd9131
samob917 <Soberly1@jhu.edu>
2022-11-13 01:10:59 +0000

    uh

--- simulation/sim.py
+++ simulation/sim.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+import seaborn as sns
 
 
 
@@ -6,8 +8,9 @@ timestep = 1
 max_current = 100
 max_charge = 1
 
-heat_pump_current = 1
-boiler_current = 1
+#current_hvac = 16.6
+#current_water = 20.8
+
 
 
 def do_one_timestep(current_state):
@@ -19,43 +22,16 @@ def do_one_timestep(current_state):
     warm_water_volume = current_state[7]
 
 
-def find_optimal_currents(time, charge, house_temperature, warm_water_volume, current_appliances):
-    # if we can afford to turn both on at once, do that
-    if current_appliances + heat_pump_current + boiler_current <= max_current:
-        current_heating = heat_pump_current
-        current_water = boiler_current
-    
-    # if we can afford to turn on both, but not at once, figure out which is worthier 
-    elif (current_appliances + heat_pump_current <= max_current) and (current_appliances + boiler_current <= max_current):
-        # evaluate which one is worth more and turn that on
-    
-    # if we can only afford to turn on the heating, do that
-    elif current_appliances + heat_pump_current <= max_current:
-        current_heating = heat_pump_current
-        current_water = 0
-    
-    # if we can only afford to turn on the boiler, do that
-    elif current_appliances + boiler_current <= max_current:
-        current_heating = 0
-        current_water = boiler_current
-    
-    # if we can't afford to turn on either, keep both off
-    else:
-        current_heating = 0
-        current_water = 0
-    
-    # ev current is leftover if ev not fully charged yet
-    if charge < max_charge:
-        current_ev = max_current - (current_heating + current_water)
-    else:
-        current_ev = 0
 
-    return (current_ev, current_heating, current_water)
+
 
 
 def calculate_inconvenience(time, charge, house_temperature, warm_water_volume):
     return charge_inconvenience(time, charge) + house_temperature_inconvenience(time, house_temperature) + warm_water_volume_inconvenience(time, warm_water_volume)
+def hvac_inconvenience(time, house_temperature):
 
+
+    return 
 def charge_inconvenience(time, charge):
     return 1
 
@@ -65,12 +41,200 @@ def house_temperature_inconvenience(time, house_temperature):
 def warm_water_volume_inconvenience(time, warm_water_volume):
     return 1 
 
+# The following three functions get the housetemp, change in t, and cost as a result
+def heating_function_hvac(time,time_step, house_temp, current_hvac, house_size, outside_temp):
+    new_house_temp = house_temp + ((current_hvac * time_step)/(0.9 * house_size)) + (house_temp-outside_temp)*(-10 * time_step * house_size)
+    return new_house_temp
+def get_delta_t_hvac(new_house_temp, outside_temp):
+    delta_t = ((1/3) * outside_temp + 15)-new_house_temp
+    return delta_t 
+def cost_function_hvac(time, delta_t, new_house_temp, outside_temp):
+    if 0 <= time <= 240:
+        cost = 0.2
+    elif 241 <= time <= 420:
+        cost = (0.8/240)*delta_t +0.2
+    elif 421 <= time <= 480:
+        cost = 1
+    elif 481 <= time <= 540:
+        cost = -(0.5/60)*delta_t + 4.5
+    elif 541 <= time <= 960:
+        cost = 0.5
+    elif 961 <= time <= 1020:
+        cost = -(0.5/60)*delta_t - 8.5 
+    elif 1021 <= time <= 1320:
+        cost = 1
+    elif 1321 <= time <= 1440:
+        cost = -(1/120)*delta_t + 12
+    if new_house_temp > ((1/3) * outside_temp + 15):
+        cost = 0
+    return cost 
+
+
+def heating_function_water(time, time_step, water_temp, current_water, tank_size, house_temp):
+    new_water_temp = water_temp + ((current_water * time_step)/( 4.2 * tank_size)) + (water_temp-house_temp)*(-2 * time_step * tank_size)
+    return new_water_temp
+def get_delta_t_water(new_water_temp):
+    delta_t = 60 - new_water_temp 
+    return delta_t 
+def cost_function_water(time, delta_t,new_water_temp):
+    if 0 <= time <= 240:
+        cost = 0.2
+    elif 241 <= time <= 420:
+        cost = (0.8/240)*delta_t +0.2
+    elif 421 <= time <= 480:
+        cost = 1
+    elif 481 <= time <= 540:
+        cost = -(0.5/60)*delta_t + 4.5
+    elif 541 <= time <= 960:
+        cost = 0.5
+    elif 961 <= time <= 1020:
+        cost = -(0.5/60)*delta_t - 8.5 
+    elif 1021 <= time <= 1320:
+        cost = 1
+    elif 1321 <= time <= 1440:
+        cost = -(1/120)*delta_t + 12
+    if new_water_temp >= 60:
+        cost = 0
+    return cost 
+
+def cost_ev_func(t):
+    return t
+
+def get_quick_cost_water(time, time_step, water_temp, current_water, tank_size, house_temp):
+    new_water_temp = heating_function_water(time, time_step, water_temp, current_water, tank_size, house_temp)
+    delta_t_water = get_delta_t_water(new_water_temp)
+    cost_water = cost_function_water(time, delta_t_water ,new_water_temp)
+    return cost_water
+
+def get_quick_cost_hvac(time, time_step, house_temp, current_hvac, house_size, outside_temp):
+    new_house_temp = heating_function_hvac(time, time_step, house_temp, current_hvac, house_size, outside_temp)
+    delta_t_hvac = get_delta_t_hvac(new_house_temp, outside_temp)
+    cost_hvac = cost_function_hvac(time, delta_t_hvac, new_house_temp, outside_temp)
+    return cost_hvac
+
+def find_optimal_currents(time, time_step, charge, house_temp, water_temp, current_hvac, current_water, tank_size, house_size, outside_temp, current_appliances):
+    # if we can afford to turn both on at once, do that
+    cost_water = 0
+    cost_hvac = 0
+    if current_appliances + current_hvac + current_water <= charge:
+        cost_water = get_quick_cost_water(time, time_step, water_temp, current_water, tank_size, house_temp)
+        cost_hvac = get_quick_cost_hvac(time, time_step, house_temp, current_hvac, house_size, outside_temp)
+        if cost_water <= 0 and cost_hvac > 0:
+            current_water = 0
+            new_water_temp = heating_function_water(time, time_step, water_temp, 0, tank_size, house_temp)
+            current_hvac = current_hvac
+            new_house_temp = heating_function_hvac(time, time_step, house_temp, current_hvac, house_size, outside_temp)
+        elif cost_water <= 0 and cost_hvac <= 0:
+            current_water = 0
+            new_water_temp = heating_function_water(time, time_step, water_temp, 0, tank_size, house_temp)
+            current_hvac = 0
+            new_house_temp = heating_function_hvac(time, time_step, house_temp, 0, house_size, outside_temp)
+        elif cost_water > 0 and cost_hvac <= 0:
+            current_water = current_water
+            new_water_temp = heating_function_water(time, time_step, water_temp, current_water, tank_size, house_temp)
+        
+            current_hvac = current_hvac
+            new_house_temp = heating_function_hvac(time, time_step, house_temp, 0, house_size, outside_temp)
+        elif cost_water > 0 and cost_hvac > 0:
+            current_water = current_water
+            new_water_temp = heating_function_water(time, time_step, water_temp, current_water, tank_size, house_temp)
+        
+            current_hvac = current_hvac
+            new_house_temp = heating_function_hvac(time, time_step, house_temp, current_hvac, house_size, outside_temp)
+    
+    # if we can afford to turn on both, but not at once, figure out which is worthier 
+    elif (current_appliances + current_water <= charge) and (current_appliances + current_hvac <= charge):
+        # evaluate which one is worth more and turn that on
+        cost_ev = cost_ev_func(time)
+
+
+        new_house_temp = heating_function_hvac(time, time_step, house_temp, current_hvac, house_size, outside_temp)
+        delta_t_hvac = get_delta_t_hvac(new_house_temp, outside_temp)
+        cost_hvac = cost_function_hvac(time, delta_t_hvac,new_house_temp,outside_temp)
+
+        new_water_temp = heating_function_water(time, time_step, water_temp, current_water, tank_size, house_temp)
+        delta_t_water = get_delta_t_water(new_water_temp)
+        cost_water = cost_function_water(time, delta_t_water,new_water_temp)
+
+        if cost_hvac < cost_water and cost_water > 0 :
+            current_water = current_water
+            new_water_temp = heating_function_water(time,time_step,  water_temp, current_water, tank_size, house_temp)
+            current_hvac = 0
+            new_house_temp = heating_function_hvac(time, time_step, house_temp, 0, house_size, outside_temp)
+        elif cost_hvac > 0 :
+            current_hvac = current_hvac
+            new_house_temp = heating_function_hvac(time,time_step,  house_temp, current_hvac, house_size, outside_temp)
+            current_water = 0
+            new_water_temp = heating_function_water(time, time_step, water_temp, 0, tank_size, house_temp)
+
+    # if we can only afford to turn on the heating, do that
+    elif current_appliances + current_hvac <= charge:
+        
+        cost_hvac = get_quick_cost_hvac(time, time_step, house_temp, current_hvac, house_size, outside_temp)
+        if cost_hvac <= 0:
+            current_hvac = 0
+            new_house_temp = heating_function_hvac(time,time_step, house_temp, 0, house_size, outside_temp)
+            current_water = 0
+            new_water_temp = heating_function_water(time,time_step, water_temp, 0, tank_size, house_temp)
+        else: 
+            current_hvac = current_hvac
+            new_house_temp = heating_function_hvac(time,time_step, house_temp, current_hvac, house_size, outside_temp)
+            current_water = 0
+            new_water_temp = heating_function_water(time,time_step, water_temp, 0, tank_size, house_temp)
+
+    
+    # if we can only afford to turn on the boiler, do that
+    elif current_appliances + current_water <= charge:
+        cost_water = get_quick_cost_water(time, time_step, water_temp, current_water, tank_size, house_temp)
+        if cost_water <= 0:
+            current_water = 0
+            new_water_temp = heating_function_water(time, time_step,water_temp, 0, tank_size, house_temp)
+            current_hvac = 0
+            new_house_temp = heating_function_hvac(time, time_step,house_temp, 0, house_size, outside_temp)
+        else:
+            current_water = current_water
+            new_water_temp = heating_function_water(time, time_step,water_temp, current_water, tank_size, house_temp)
+            current_hvac = 0
+            new_house_temp = heating_function_hvac(time, time_step,house_temp, 0, house_size, outside_temp)
+    # if we can't afford to turn on either, keep both off
+    else:
+        current_hvac = 0
+        new_house_temp = heating_function_hvac(time,time_step, house_temp, 0, house_size, outside_temp)
+        current_water = 0
+        new_water_temp = heating_function_water(time,time_step, water_temp, 0, tank_size, house_temp)
+    
+    # ev current is leftover if ev not fully charged yet
+    if current_water + current_appliances + current_hvac < charge:
+        current_ev = charge - (current_hvac + current_water + current_appliances)
+    else:
+        current_ev = 0
+    
+    cost_ev = cost_ev_func(time)
+
+    
+    
+
+    return (current_ev, current_hvac, current_water, new_house_temp, new_water_temp, cost_hvac, cost_ev, cost_water)
+
+def simulation(time_step, charge, house_temp, water_temp, current_hvac, current_water, house_size, tank_size, outside_temp, current_appliances):
+    new_house_temp = house_temp
+    new_water_temp = water_temp
+    #results = [[0,charge-current_appliances, 0, 0, new_house_temp, new_water_temp, 0,0,0]]
+    results = []
+    for t in range(0,1440, timestep):
+        current_ev, current_hvac, current_water, new_house_temp, new_water_temp, cost_hvac, cost_ev, cost_water= find_optimal_currents(t, time_step, charge, new_house_temp, new_water_temp, current_hvac, current_water, house_size,tank_size, outside_temp, current_appliances)
+        results.append([t,current_ev,current_hvac, current_water,new_house_temp,new_water_temp,cost_hvac, cost_ev, cost_water ])
+    df = pd.DataFrame(results, columns=["time","current_ev", "current_hvac", "current_water", "house_temp", "water_temp", "cost_hvac", "cost_ev", "cost_water"])
+    return df
+
+def plot_lines(df):
+    return 
+
+
 if __name__ == '__main__':
-    # time, current_ev, current_heating, current_water, charge, house_temperature, warm_water_volume
-    time = 0
-    charge = 1
-    house_temperature = 1
-    warm_water_volume = 1
-    output = pd.DataFrame()
-    for time
-    output.concat([1,2,3])
+    df = simulation(time_step=1, charge=100,house_temp = 16,water_temp = 55, current_hvac = 16.6, current_water=20.8, house_size=555.24,tank_size=200 ,outside_temp=7, current_appliances=70)
+    print(df)
+    plot = sns.lineplot(data=df[["water_temp","house_temp", "cost_hvac", "cost_water"]])
+    fig = plot.get_figure()
+    fig.savefig("out.png")
+    df.to_csv("problem.csv")
--- visualization/viz.py
+++ visualization/viz.py
@@ -1,7 +1,76 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn
+import pandas as pd
+import xml.etree.ElementTree as et
+
+def parse_XML(xml_file, df_cols): 
+    """Parse the input XML file and store the result in a pandas 
+    DataFrame with the given columns. 
+    
+    The first element of df_cols is supposed to be the identifier 
+    variable, which is an attribute of each node element in the 
+    XML data; other features will be parsed from the text content 
+    of each sub-element. 
+    """
+    
+    xtree = et.parse(xml_file)
+    xroot = xtree.getroot()
+    rows = []
+    
+    for node in xroot: 
+        res = []
+        res.append(node.attrib.get(df_cols[0]))
+        for el in df_cols[1:]: 
+            if node is not None and node.find(el) is not None:
+                res.append(node.find(el).text)
+            else: 
+                res.append(None)
+        rows.append({df_cols[i]: res[i] 
+                     for i, _ in enumerate(df_cols)})
+    
+    out_df = pd.DataFrame(rows, columns=df_cols)
+        
+    return out_df
+
+
 
 
 if __name__ == '__main__':
-    print("Hello world")
+    tree = et.parse("REFIT_BUILDING_SURVEY.xml")
+    root = tree.getroot()
+    print()
+    print("-------------- LEVEL 1 -------------------")
+    print()
+    for child in root: 
+        print(child.tag, child.attrib)
+        print()
+        print("-------------- LEVEL 2 -------------------")
+        print()
+        for gchild in child:
+            print(gchild.tag, gchild.attrib)
+            print()
+            print("-------------- LEVEL 3 -------------------")
+            print()
+            for ggchild in gchild:
+                print(ggchild.tag, ggchild.attrib)
+                print()
+                print("-------------- LEVEL 4 -------------------")
+                print()
+                for gggchild in ggchild:
+                    print(gggchild.tag, gggchild.attrib)
+                    print()
+                    print("-------------- LEVEL 5 -------------------")
+                    print()
+                    for g4child in gggchild:
+                        print(g4child.tag, g4child.attrib)
+                        print()
+                        print("-------------- LEVEL 6 -------------------")
+                        print()
+                        for g5child in g4child:
+                             print(g5child.tag, g5child.attrib)
+                             print()
+                             print("-------------- LEVEL 7 -------------------")
+                             print()
+                             for g6child in g5child:
+                                print(g6child.tag, g6child.attrib)
